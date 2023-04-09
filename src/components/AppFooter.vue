c70f00a540be0bab363c2bbdd527a347fe82e573
Dominic Cartwright <dcartwright07@users.noreply.github.com>
2023-04-09 15:32:35 -0400

    fix: Add padding on bottom on main tag

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="mt-10">
-    <footer class="bg-[#1d1d1d] border-t-4 border-red-700 fixed bottom-0 w-full">
-      <div class="mx-auto max-w-7xl flex justify-start py-4">
-        <p class="text-sm text-white">Built by: Dominic Cartwright | Slingshot Project</p>
-      </div>
-    </footer>
-  </div>
+  <footer class="bg-[#1d1d1d] border-t-4 border-red-700 fixed bottom-0 w-full">
+    <div class="mx-auto max-w-7xl flex justify-start py-4">
+      <p class="text-sm text-white">Built by: Dominic Cartwright | Slingshot Project</p>
+    </div>
+  </footer>
 </template>
