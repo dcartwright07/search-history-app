9daab1c63d4b19b4dd635eabea87b60e29571dc0
Dominic Cartwright <dcartwright07@users.noreply.github.com>
2023-04-09 15:27:33 -0400

    fix: Footer now sticks to the bottom of screen

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <footer class="bg-[#1d1d1d] border-t-4 border-red-700">
+  <footer class="bg-[#1d1d1d] border-t-4 border-red-700 absolute bottom-0 w-full">
     <div class="mx-auto max-w-7xl flex justify-start py-4">
       <p class="text-sm text-white">Built by: Dominic Cartwright | Slingshot Project</p>
     </div>
